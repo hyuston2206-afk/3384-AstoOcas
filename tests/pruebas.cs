92debe09de8221e1a16e36749c3dac5b5798d043
hyuston <N00496841@upn.pe>
2025-09-20 18:37:07 -0500

    🧪 Prueba básica de suma agregada

--- tests/pruebas.cs
+++ tests/pruebas.cs
@@ -1,1 +1,14 @@
 // Aquí irán las pruebas
+using System;
+
+namespace pruebas
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prueba de suma: 2 + 3 = " + (2 + 3));
+        }
+    }
+}
+
