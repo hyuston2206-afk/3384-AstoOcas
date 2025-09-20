7968e3d8ab2a3e4f36d076164829d9a4e65954dc
hyuston <N00496841@upn.pe>
2025-09-20 17:23:08 -0500

    Agregue archivos fuente en C# dentro de la carpeta src

---
+++ src/controlBasico.cs
@@ -0,0 +1,19 @@
+// controlBasico.cs
+// Autor: Hyuston
+// Fecha: 20/09/2025
+// Este archivo contiene ejemplos básicos de estructuras de control en C#
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        MostrarMensaje();
+    }
+
+    static void MostrarMensaje()
+    {
+        Console.WriteLine("Hola, este es un mensaje desde controlBasico.cs");
+    }
+}
