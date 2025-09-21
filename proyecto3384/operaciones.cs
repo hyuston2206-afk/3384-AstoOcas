579f30c4a091cefc5d0430ec9570ac408b00f074
hyuston <N00496841@upn.pe>
2025-09-20 19:53:15 -0500

    Probé el programa y confirmé que funciona correctamente

--- proyecto3384/operaciones.cs
+++ proyecto3384/operaciones.cs
@@ -8,5 +8,14 @@ namespace proyecto3384
 {
     internal class operaciones
     {
+        public static int Sumar(int a, int b) => a + b;
+        public static int Restar(int a, int b) => a - b;
+        public static int Multiplicar(int a, int b) => a * b;
+        public static int Dividir(int a, int b)
+        {
+            if (b == 0) throw new DivideByZeroException();
+            return a / b;
+        }
+
     }
 }
